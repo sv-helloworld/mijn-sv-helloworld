59195b213acaf0d70d5456b44210e61d41338ecf
Raymon de Looff <raydelooff@gmail.com>
2016-06-19 19:51:31 +0200

    Concatenate JS and CSS to single files

--- gulpfile.js
+++ gulpfile.js
@@ -6,50 +6,81 @@ var gulp = require('gulp'),
         autoprefixer = require('gulp-autoprefixer'),
         livereload = require('gulp-livereload'),
         uglify = require('gulp-uglify'),
-        rename = require('gulp-rename');
+        concat = require('gulp-concat'),
+        concatCss = require('gulp-concat-css');
+
+var stylesheets = [
+    'resources/assets/css/**/*.css'
+];
+
+var scripts = {
+    app: [
+        'resources/assets/js/**/*.js',
+    ],
+    vendor: [
+        'public/bower_components/jquery/dist/jquery.min.js',
+    ],
+};
 
 gulp.task('blade', function() {
-    return gulp.src('./resources/views/**/*.blade.php')
+    return gulp.src('resources/views/**/*.blade.php')
         .pipe(livereload());
 });
 
 gulp.task('sass', function() {
-    return gulp.src('./resources/assets/sass/**/*.{sass,scss}')
+    return gulp.src('resources/assets/sass/**/*.{sass,scss}')
         .pipe(sass({outputStyle: 'compressed'}).on('error', function(error) {
             gutil.log(error);
             this.emit('end');
         }))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('./public/css'))
+        .pipe(gulp.dest('resources/assets/css'))
         .pipe(livereload());
 });
 
-gulp.task('compress', function() {
-    return gulp.src('./resources/assets/js/*.js')
+gulp.task('css', function() {
+    return gulp.src(stylesheets)
+        .pipe(concatCss('app.min.css'))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('scripts_app', function() {
+    return gulp.src(scripts.app)
         .pipe(uglify().on('error', function(error) {
             gutil.log(error);
             this.emit('end');
         }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./public/js'))
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('public/js'))
         .pipe(livereload());
 });
 
-gulp.task('watch', function () {
+gulp.task('scripts_vendor', function() {
+    return gulp.src(scripts.vendor)
+        .pipe(uglify().on('error', function(error) {
+            gutil.log(error);
+            this.emit('end');
+        }))
+        .pipe(concat('vendor.min.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
     livereload.listen({
         host: 'localhost'
     });
 
     // Watch Blade files
-    gulp.watch('./resources/views/**/*.blade.php', ['blade']);
+    gulp.watch('resources/views/**/*.blade.php', ['blade']);
 
     // Watch SASS and SCSS files
-    gulp.watch('./resources/assets/sass/**/*.{sass,scss}', ['sass']);
+    gulp.watch('resources/assets/sass/**/*.{sass,scss}', ['sass', 'css']);
 
     // Watch JS files
-    gulp.watch('./resources/assets/js/*.js', ['compress']);
+    gulp.watch('resources/assets/js/*.js', ['scripts_app']);
 });
+
+gulp.task('vendor', ['scripts_vendor'])
 
 gulp.task('default', ['watch']);
