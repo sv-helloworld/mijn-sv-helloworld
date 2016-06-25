db913316f6714ce5dbafac2f22c0e2abb740ff68
Thijs Clowting <thijs@clowting.me>
2016-06-25 17:55:23 +0200

    Tests, work in progress

---
+++ database/seeds/TestingDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class TestingDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $this->call(UserCategoriesSeeder::class);
+
+        Model::reguard();
+    }
+}
