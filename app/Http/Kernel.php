862522c6fedb5aa3d706fb7642d85c173a86f2cb
Raymon de Looff <raydelooff@gmail.com>
2016-06-25 16:33:44 +0200

    Add SidebarMenu middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\MainMenu::class,
+            \App\Http\Middleware\SidebarMenu::class,
         ],
 
         'api' => [
---
+++ app/Http/Middleware/SidebarMenu.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Menu;
+use Auth;
+
+class SidebarMenu
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        Menu::make('sidebar', function ($menu) {
+        	// Add items in specific controllers
+        });
+
+        return $next($request);
+    }
+}
