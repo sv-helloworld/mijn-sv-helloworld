a169d2fe2159bcd9c7dc76eda3da754de4135540
Thijs Clowting <thijs@clowting.me>
2016-08-27 20:36:32 +0200

    Add Period model and migration, #18 closed

---
+++ app/Period.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Period extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'start_date', 'end_date',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+}
