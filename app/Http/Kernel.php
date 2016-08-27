f88eff0ace0e2859b43d58eac8771ca9b9557e25
Raymon de Looff <raydelooff@gmail.com>
2016-08-27 15:34:36 +0200

    Fix duplicate middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,10 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \App\Http\Middleware\MainMenu::class,
     ];
 
     /**
@@ -34,6 +30,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\MainMenu::class,
             \App\Http\Middleware\SidebarMenu::class,
         ],
 
