80886fde29cd9586511f71d85f1d7c2871b1a027
Raymon de Looff <raydelooff@gmail.com>
2016-08-28 20:57:14 +0200

    Add index content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,36 @@
 @extends('layouts.master')
-@section('title', 'Welkom')
+@section('title', 'Mijn SV "Hello World"')
+
+@section('content')
+    <p>
+        Mijn SV "Hello World" is het portaal voor alle leden en niet-leden van de opleiding HBO-ICT van de HZ University of Applied Sciences.
+        Op dit moment is het portaal nog enigszins beperkt qua functionaliteit.
+        Je kunt je op dit moment in ieder geval al lid worden en de contributie via iDeal betalen, makkelijk!
+    </p>
+
+    <p>
+        Gaandeweg zal er steeds meer functionaliteit aan dit portaal toegevoegd worden.
+        In de toekomst willen we het ook mogelijk maken om je aan te melden voor de activiteiten die we zullen organiseren.
+        Hierbij is het ook de bedoeling dat iedereen, zowel leden als niet-leden,
+        zich kunnen aanmelden en direct kunnen betalen voor de deelname aan het activiteit indien het activiteit niet vrij toegankelijk is.
+    </p>
+
+    <p>
+        Problemen die optreden in ons portaal worden op de achtergrond gerapporteerd.
+        Wanneer er een fout zich voordoet, wat goed mogelijk is,
+        krijg je de mogelijkheid een korte beschrijving te sturen over hoe je dit probleem bent tegengekomen.
+        Het invullen van deze informatie is uiteraard niet verplicht,
+        maar je kan ons op deze manier helpen zodat dat we het probleem sneller kunnen oplossen!
+    </p>
+
+    <p>
+        Klachten, opmerkingen en aanvullingen zijn ook welkom!
+        Deze mag je indienen via de <a href="http://svhelloworld.nl/contact-opnemen/" target="_blank">contactpagina</a>,
+        of maak een nieuwe issue aan op <a href="https://github.com/sv-helloworld/mijn-sv-helloworld" target="_blank">onze GitHub-pagina</a>.
+    </p>
+
+    <p>
+        Met vriendelijke groet,<br>
+        Studievereniging "Hello World"
+    </p>
+@endsection
