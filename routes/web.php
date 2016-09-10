4cc13e581ae23a3f44698e891d4b81552b7fe8d7
Raymon de Looff <raydelooff@gmail.com>
2016-09-10 12:18:37 +0200

    Move payment routes from admin middleware

--- routes/web.php
+++ routes/web.php
@@ -44,24 +44,6 @@ Route::group(['middleware' => ['auth']], function () {
                 ]);
             });
 
-            // Payments
-            Route::group(['prefix' => 'betaling', 'as' => 'payment.'], function () {
-                Route::get('/', [
-                    'uses' => 'PaymentController@index',
-                    'as' => 'index',
-                ]);
-
-                Route::get('{payment}', [
-                    'uses' => 'PaymentController@show',
-                    'as' => 'show',
-                ]);
-
-                Route::get('{payment}/betalen', [
-                    'uses' => 'PaymentController@pay',
-                    'as' => 'pay',
-                ]);
-            });
-
             // User management routes
             Route::patch('gebruikers/{user}/activeren', [
                 'uses' => 'UserController@activate',
@@ -103,6 +85,24 @@ Route::group(['middleware' => ['auth']], function () {
                 'as' => 'show',
             ]);
         });
+    });
+
+    // Payments
+    Route::group(['prefix' => 'betaling', 'as' => 'payment.'], function () {
+        Route::get('/', [
+            'uses' => 'PaymentController@index',
+            'as' => 'index',
+        ]);
+
+        Route::get('{payment}', [
+            'uses' => 'PaymentController@show',
+            'as' => 'show',
+        ]);
+
+        Route::get('{payment}/betalen', [
+            'uses' => 'PaymentController@pay',
+            'as' => 'pay',
+        ]);
     });
 
     Route::group(['as' => 'account.'], function () {
