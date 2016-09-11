d50873ee6b7f63b02b4eaf2995d5fb08ed0fc13e
Raymon de Looff <raydelooff@gmail.com>
2016-09-11 12:52:26 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- app/Listeners/SendSubscriptionApprovedNotification.php
+++ app/Listeners/SendSubscriptionApprovedNotification.php
@@ -3,8 +3,6 @@
 namespace App\Listeners;
 
 use App\Events\SubscriptionApproved;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Notifications\SubscriptionApproved as SubscriptionApprovedNotification;
 
 class SendSubscriptionApprovedNotification
