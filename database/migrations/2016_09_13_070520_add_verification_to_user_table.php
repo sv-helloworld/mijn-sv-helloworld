5796b409f5564303b72ea7e92d838b36fcc3ff5a
Raymon de Looff <raydelooff@gmail.com>
2016-10-01 16:12:42 +0200

    Add custom migration for laravel-user-verification

---
+++ database/migrations/2016_09_13_070520_add_verification_to_user_table.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of Jrean\UserVerification package.
+ *
+ * (c) Jean Ragouin <go@askjong.com> <www.askjong.com>
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddVerificationToUserTable extends Migration
+{
+    /**
+     * Determine the user table name.
+     *
+     * @return string
+     */
+    public function getUserTableName()
+    {
+        $user_model = config('auth.providers.users.model', App\User::class);
+
+        return (new $user_model)->getTable();
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->getUserTableName(), function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->getUserTableName(), function (Blueprint $table) {
+            //
+        });
+    }
+}
