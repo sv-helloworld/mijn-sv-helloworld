5ce3fa1ecda129579ab59518368ec9cc05093bf3
Raymon de Looff <raydelooff@gmail.com>
2016-10-04 00:06:44 +0200

    Remove unused user API route

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-
 Route::post('betaling/webhook', [
     'uses' => 'PaymentController@webhook',
     'as' => 'payment.webhook',
