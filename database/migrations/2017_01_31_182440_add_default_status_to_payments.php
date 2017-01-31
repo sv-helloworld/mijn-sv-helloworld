c9a98fab92e5a7c4da9f863950b84269343d936e
Raymon de Looff <raydelooff@gmail.com>
2017-01-31 18:29:58 +0100

    Apply fixes from StyleCI (#94)
    [ci skip] [skip ci]

--- database/migrations/2017_01_31_182440_add_default_status_to_payments.php
+++ database/migrations/2017_01_31_182440_add_default_status_to_payments.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 use App\Payment;
+use Illuminate\Database\Migrations\Migration;
 
 class AddDefaultStatusToPayments extends Migration
 {
