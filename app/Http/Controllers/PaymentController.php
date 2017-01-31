2bba767f257d672a22e6aa99af7f32137855120d
Raymon de Looff <raydelooff@gmail.com>
2017-01-31 17:25:55 +0100

    Do not surpress ‘non-numeric value’ errors
    Belongs to #88

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -87,7 +87,7 @@ class PaymentController extends Controller
 
         $pdf = PDF::loadView('payment.invoice', compact('payment'));
 
-        return @$pdf->download(sprintf('factuur_%s.pdf', $payment->id));
+        return $pdf->download(sprintf('factuur_%s.pdf', $payment->id));
     }
 
     /**
