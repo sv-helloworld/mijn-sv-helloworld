4cd49d56d27381f9261a91d774f54cf2dd45f8cd
Raymon de Looff <raydelooff@gmail.com>
2017-07-23 12:09:25 +0200

    Update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,22 +4,12 @@
 @section('content')
     <p>
         Mijn SV "Hello World" is het portaal voor alle leden van Studievereniging "Hello World",
-        maar ook niet-leden zich aanmelden voor activiteiten via dit portaal!
+        maar ook niet-leden mogen zich aanmelden voor activiteiten via dit portaal!
         Je moet hiervoor wel student zijn aan de opleiding HBO-ICT van de HZ University of Applied Sciences.
     </p>
 
     <p>
-        Problemen die optreden in ons portaal worden op de achtergrond gerapporteerd.
-        Wanneer er een fout zich voordoet, wat goed mogelijk is,
-        krijg je de mogelijkheid een korte beschrijving te sturen over hoe je dit probleem bent tegengekomen.
-        Het invullen van deze informatie is uiteraard niet verplicht,
-        maar je kan ons op deze manier helpen zodat dat we het probleem sneller kunnen oplossen!
-    </p>
-
-    <p>
-        Klachten, opmerkingen en aanvullingen zijn ook welkom!
-        Deze mag je indienen via de <a href="http://svhelloworld.nl/contact-opnemen/" target="_blank">contactpagina</a>,
-        of maak een nieuwe issue aan op <a href="https://github.com/sv-helloworld/mijn-sv-helloworld" target="_blank">onze GitHub-pagina</a>.
+        Als je nieuwe ideeën hebt voor dit portaal mag je deze indienen op <a href="https://github.com/sv-helloworld/mijn-sv-helloworld" target="_blank">onze GitHub-pagina</a>.
     </p>
 
     <p>
